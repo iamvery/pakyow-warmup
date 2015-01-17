4a1d8a1d050cc07a778d286d5356f67f37645727
Jay Hayes <ur@iamvery.com>
2015-01-17 10:34:25 -0500

    Define helper method for twitter client

--- app/lib/helpers.rb
+++ app/lib/helpers.rb
@@ -1,3 +1,12 @@
+require 'twitter'
+
 module Pakyow::Helpers
-  # define methods here that are available from routes, bindings, etc
+  def twitter_client
+    @client ||= Twitter::REST::Client.new do |client|
+      client.consumer_key         = config.app.twitter_consumer_key
+      client.consumer_secret      = config.app.twitter_consumer_secret
+      client.access_token         = config.app.twitter_access_token
+      client.access_token_secret  = config.app.twitter_access_token_secret
+    end
+  end
 end
