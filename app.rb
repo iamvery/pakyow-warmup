7212d3ed3a9bf3bb93daff8779601865e2cf3260
Jay Hayes <ur@iamvery.com>
2015-01-17 10:32:29 -0500

    Configure twitter api access values

--- app.rb
+++ app.rb
@@ -8,6 +8,11 @@ Pakyow::App.define do
   configure :global do
     # put global config here and they'll be available across environments
     app.name = 'Pakyow'
+
+    app.twitter_consumer_key = ENV['TWITTER_CONSUMER_KEY']
+    app.twitter_consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+    app.twitter_access_token = ENV['TWITTER_ACCESS_TOKEN']
+    app.twitter_access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
   end
 
   configure :development do
